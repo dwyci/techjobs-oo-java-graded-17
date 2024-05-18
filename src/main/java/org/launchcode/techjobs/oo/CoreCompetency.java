d287be337fdbf9c67495810bae8fcf1ed8e47e71
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-05-18 13:33:18 -0500

    completed GA2

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -2,56 +2,56 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class CoreCompetency {
+public class CoreCompetency extends JobField{
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public CoreCompetency() {
-        this.id = nextId;
-        nextId++;
-    }
+//    private int id;
+//    private static int nextId = 1;
+//    private String value;
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public CoreCompetency() {
+//        this.id = nextId;
+//        nextId++;
+//    }
 
     public CoreCompetency(String value) {
-        this();
-        this.value = value;
+        super(value);
+
     }
 
     // Custom toString, equals, and hashCode methods:
 
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
-        if (this == o) return true;
-        if (!(o instanceof PositionType)) return false;
-        CoreCompetency coreCompetency = (CoreCompetency) o;
-        return getId() == coreCompetency.getId();
-    }
-
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
-    // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
-    //  ONLY a getter for the 'id' field.
+//    @Override
+//    public String toString() {
+//        return value;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+//        if (this == o) return true;
+//        if (!(o instanceof PositionType)) return false;
+//        CoreCompetency coreCompetency = (CoreCompetency) o;
+//        return getId() == coreCompetency.getId();
+//    }
+//
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
+//
+//    // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
+//    //  ONLY a getter for the 'id' field.
 
 }
